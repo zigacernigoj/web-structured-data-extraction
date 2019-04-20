fdd07aaa434c7df423481d6bbab4d7056b2f6b1b
lavrinec <ml9987@student.uni-lj.si>
2019-04-20 12:33:31 +0200

    rtvslo xpath and re test implementation

--- implementation/src/re.py
+++ implementation/src/re.py
@@ -2,7 +2,19 @@ import re
 
 
 def process_file(content):
-    regex = "(\w+)"
+    print("RE")
+    # TODO refactor
+    regex = "<h1[^>]*>(.*)<\/h1[^>]*>"
     pattern = re.compile(regex)
     match = pattern.search(content)
-    print("Found person: '{}'.".format(match.group(1)))
+    print("Found title: '{}'.".format(match.group(1)))
+    
+    regex = "<div[^>]*class=\"[^\"]*subtitle[^\"]*\"[^>]*>(.*)<\/div[^>]*>"
+    pattern = re.compile(regex)
+    match = pattern.search(content)
+    print("Found subtitle: '{}'.".format(match.group(1)))
+    
+    regex = "<[^>]*class=\"[^\"]*lead[^\"]*\"[^>]*>(.*)<\/[^>]*>"
+    pattern = re.compile(regex)
+    match = pattern.search(content)
+    print("Found lead: '{}'.".format(match.group(1)))
--- implementation/src/xpath.py
+++ implementation/src/xpath.py
@@ -2,7 +2,12 @@ from lxml import html
 
 
 def process_file(content):
+    print("XPATH")
     tree = html.fromstring(content)
-    title = str(tree.xpath('//h1/text()')[0])
-    print("Found title: '%s'." % title)
+    title = str(tree.xpath('//*[@id="main-container"]//h1/text()')[0])
+    subtitle = str(tree.xpath('//*[@id="main-container"]//*[@class="subtitle"]/text()')[0])
+    lead = str(tree.xpath('//*[@id="main-container"]//*[@class="lead"]/text()')[0])
+    print("Found title: ", title)
+    print("Found subtitle: ", subtitle)
+    print("Found lead: ", lead)
 
