5de79b697ca725cc0ee90c666ab391695278c24f
lavrinec <ml9987@student.uni-lj.si>
2019-04-20 12:53:44 +0200

    regex and xpath for rtvslo

--- implementation/src/re.py
+++ implementation/src/re.py
@@ -1,20 +1,31 @@
 import re
 
 
+def execute_regex(content, regex):
+    pattern = re.compile(regex)
+    match = pattern.search(content)
+    if match != None:
+        return match.group(1)
+    return "Ni rezultata"
+
+
 def process_file(content):
     print("RE")
     # TODO refactor
-    regex = "<h1[^>]*>(.*)<\/h1[^>]*>"
-    pattern = re.compile(regex)
-    match = pattern.search(content)
-    print("Found title: '{}'.".format(match.group(1)))
+    result = execute_regex(content, "<h1[^>]*>(.*)<\/h1[^>]*>")
+    print("Found title: '{}'.".format(result))
     
-    regex = "<div[^>]*class=\"[^\"]*subtitle[^\"]*\"[^>]*>(.*)<\/div[^>]*>"
-    pattern = re.compile(regex)
-    match = pattern.search(content)
-    print("Found subtitle: '{}'.".format(match.group(1)))
+    result = execute_regex(content, "<div[^>]*class=\"[^\"]*subtitle[^\"]*\"[^>]*>(.*)<\/div[^>]*>")
+    print("Found subtitle: '{}'.".format(result))
     
-    regex = "<[^>]*class=\"[^\"]*lead[^\"]*\"[^>]*>(.*)<\/[^>]*>"
-    pattern = re.compile(regex)
-    match = pattern.search(content)
-    print("Found lead: '{}'.".format(match.group(1)))
+    result = execute_regex(content, "<[^>]*class=\"[^\"]*lead[^\"]*\"[^>]*>(.*)<\/[^>]*>")
+    print("Found lead: '{}'.".format(result))
+    
+    result = execute_regex(content, "<[^>]*class=\"[^\"]*author-name[^\"]*\"[^>]*>(.*)<\/[^>]*>")
+    print("Found author name: '{}'.".format(result))
+    
+    result = execute_regex(content, "<[^>]*class=\"[^\"]*publish-meta[^\"]*\"[^>]*>(.*)<\/[^>]*>")
+    print("Found publish meta: '{}'.".format(result))
+    
+    result = execute_regex(content, "<[^>]*class=\"[^\"]*article-body[^\"]*\"[^>]*>(.*)<\/[^>]*>")
+    print("Found article body: '{}'.".format(result))
--- implementation/src/xpath.py
+++ implementation/src/xpath.py
@@ -7,7 +7,13 @@ def process_file(content):
     title = str(tree.xpath('//*[@id="main-container"]//h1/text()')[0])
     subtitle = str(tree.xpath('//*[@id="main-container"]//*[@class="subtitle"]/text()')[0])
     lead = str(tree.xpath('//*[@id="main-container"]//*[@class="lead"]/text()')[0])
+    author_name = str(tree.xpath('//*[@id="main-container"]//*[@class="author-name"]/text()')[0])
+    publish_meta = str(tree.xpath('//*[@id="main-container"]//*[@class="publish-meta"]/text()')[0])
+    article_body = str(tree.xpath('//*[@id="main-container"]//*[@class="article-body"]/text()')[0])
     print("Found title: ", title)
     print("Found subtitle: ", subtitle)
     print("Found lead: ", lead)
+    print("Found author name: ", author_name)
+    print("Found publish meta: ", publish_meta)
+    print("Found article body: ", article_body)
 
